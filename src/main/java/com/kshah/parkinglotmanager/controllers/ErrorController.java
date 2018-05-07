c1c76937f8ec629b755584ad64592f9ed57160a7
Keval Shah <kevalshah93@gmail.com>
2018-05-06 19:01:52 -0700

    Changed log levels in ErrorController

--- src/main/java/com/kshah/parkinglotmanager/controllers/ErrorController.java
+++ src/main/java/com/kshah/parkinglotmanager/controllers/ErrorController.java
@@ -25,23 +25,23 @@ import java.util.List;
 @ControllerAdvice(annotations = RestController.class)
 public class ErrorController {
 
-    public static final String RESOURCE_NOT_FOUND_MSG = "Resource not found";
-    public static final String CONTENT_TYPE_UNSUPPORTED_MSG = "Specified content type is not supported";
-    public static final String VALIDATION_ERROR_MSG = "Validation failed on request";
-    public static final String HTTP_MESSAGE_UNREADABLE_MSG = "Malformed request";
-    public static final String INTERNAL_SERVER_ERROR_MSG = "Internal server error";
+    private static final String RESOURCE_NOT_FOUND_MSG = "Resource not found";
+    private static final String CONTENT_TYPE_UNSUPPORTED_MSG = "Specified content type is not supported";
+    private static final String VALIDATION_ERROR_MSG = "Validation failed on request";
+    private static final String HTTP_MESSAGE_UNREADABLE_MSG = "Malformed request";
+    private static final String INTERNAL_SERVER_ERROR_MSG = "Internal server error";
 
 
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public ResponseEntity<Error> contentTypeNotSupported(final HttpMediaTypeNotSupportedException e) {
-        log.error("Content type not supported in request body.", e);
+        log.warn("Content type not supported in request body.", e);
         return createErrorResponse(HttpStatus.UNSUPPORTED_MEDIA_TYPE, CONTENT_TYPE_UNSUPPORTED_MSG, Collections.singletonList(e.getMessage()));
     }
 
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Error> httpMessageUnreadable(final HttpMessageNotReadableException e) {
-        log.error("HTTP message is unreadable", e);
+        log.warn("HTTP message is unreadable", e);
         return createErrorResponse(HttpStatus.BAD_REQUEST, HTTP_MESSAGE_UNREADABLE_MSG);
     }
 
@@ -55,21 +55,21 @@ public class ErrorController {
 
     @ExceptionHandler(ParkingLotCapacityReachedException.class)
     public ResponseEntity parkingLotCapacityReached(final ParkingLotCapacityReachedException e) {
-        log.error("Parking lot capacity has been reached.", e);
+        log.info("Parking lot capacity has been reached.", e);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Error> resourceNotFound(final ResourceNotFoundException e) {
-        log.error("The requested resource was not found.", e);
+        log.warn("The requested resource was not found.", e);
         return createErrorResponse(HttpStatus.NOT_FOUND, RESOURCE_NOT_FOUND_MSG, Collections.singletonList(e.getMessage()));
     }
 
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Error> validationError(final MethodArgumentNotValidException e) {
-        log.error("Validation error occurred on request.", e);
+        log.warn("Validation error occurred on request.", e);
         List<String> errors = new ArrayList<>();
         List<ObjectError> validationErrors = e.getBindingResult().getAllErrors();
         for (int i = 0; i < validationErrors.size(); i++) {
@@ -81,7 +81,7 @@ public class ErrorController {
 
     @ExceptionHandler(BadDataException.class)
     public ResponseEntity<Error> validationError(final BadDataException e) {
-        log.error("Validation error occurred on request.", e);
+        log.warn("Validation error occurred on request.", e);
         return createErrorResponse(HttpStatus.BAD_REQUEST, VALIDATION_ERROR_MSG, Collections.singletonList(e.getMessage()));
     }
 
